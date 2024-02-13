5d25a4d8088d53c7cb8dd3cb04467f5de21cf568
zholobko_an <zholobko_an@orlan.in>
2024-02-13 13:58:32 +0300

    count button

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,18 @@
 // import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js'
 
 // Создайте Vue приложение
+
+
+createApp({
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.count +=1
+    }
+  }
+}).mount('#app');
