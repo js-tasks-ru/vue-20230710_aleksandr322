6bc31a901077d00c4b8706a782b822b46c3229df
zholobko_an <zholobko_an@orlan.in>
2024-02-18 01:54:29 +0300

    calculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,26 @@
 // import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js'
 
 // Создайте Vue приложение
+const calculatorOperators = {
+  sum: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => a / b,
+};
+
+createApp({
+  data() {
+    return {
+      a: 10,
+      b: 0,
+      operator: 'sum',
+    };
+  },
+
+  computed: {
+    result() {
+      return calculatorOperators[this.operator]?.(this.a, this.b);
+    },
+  },
+}).mount('#app');
